e177253df84dedc446cddce7afac1b6e57cb55cd
m-karakus <s.metinkarakus@gmail.com>
2026-02-09 14:16:17 +0300

    new structure

--- mkpipe_extractor_mariadb/__init__.py
+++ mkpipe_extractor_mariadb/__init__.py
@@ -1,14 +1,13 @@
-from mkpipe.functions_spark import BaseExtractor
+from mkpipe.spark import JdbcExtractor
 
 
-class MariadbExtractor(BaseExtractor):
-    def __init__(self, config, settings):
-        super().__init__(
-            config,
-            settings,
-            driver_name='mariadb',
-            driver_jdbc='org.mariadb.jdbc.Driver',
-        )
+class MariadbExtractor(JdbcExtractor, variant='mariadb'):
+    driver_name = 'mariadb'
+    driver_jdbc = 'org.mariadb.jdbc.Driver'
 
     def build_jdbc_url(self):
-        return f'jdbc:{self.driver_name}://{self.host}:{self.port}/{self.database}?user={self.username}&password={self.password}&sessionVariables=sql_mode=ANSI_QUOTES'
+        return (
+            f'jdbc:{self.driver_name}://{self.host}:{self.port}/{self.database}'
+            f'?user={self.username}&password={self.password}'
+            f'&sessionVariables=sql_mode=ANSI_QUOTES'
+        )
